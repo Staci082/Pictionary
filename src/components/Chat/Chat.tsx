d7cdc023870957fdfc46379285f2bbbc370f6fed
Staci082 <125351559+Staci082@users.noreply.github.com>
2023-10-16 22:58:47 +0200

    chat history added

--- src/components/Chat/Chat.tsx
+++ src/components/Chat/Chat.tsx
@@ -1,44 +1,58 @@
-// import { IoSend } from "react-icons/io5";
-import io from "socket.io-client"
-import { useState, useEffect } from "react";
+import io from "socket.io-client";
+import { useState } from "react";
 
-const socket = io("http://localhost:5172")
+const socket = io("http://localhost:5172");
 
 function Chat() {
-  const [message, setMessage] = useState<string>("")
-  const [messageReceived, setMessageReceived] = useState<string>("")
-    const sendMessage = () => {
-      socket.emit("send_message", { message })
+  const [message, setMessage] = useState<string>("");
+  const [chatHistory, setChatHistory] = useState<string[]>([]);
+
+  const sendMessage = () => {
+    if (message.trim() === "") {
+      // Don't send empty messages
+      return;
     }
 
-    useEffect(() => {
-      socket.on("received_message", (data) => {
-        setMessageReceived(data.message)
-      })
-    }, [socket])
+    // Append your message to the chat history
+    setChatHistory([...chatHistory, "You: " + message]);
 
-    return (
-        <>
-                <div className="w-[96%] h-[75%] absolute top-[8px] right-0 left-0 m-auto bg-white rounded-md">
-                {messageReceived}
-                </div>
-                <label htmlFor="chatInput"></label>
+    // Emit the message to the server
+    socket.emit("send_message", { message });
 
-                <input 
-                type="text" 
-                name="chatInput" 
-                onChange={(e) => {
-                  setMessage(e.target.value);
-                }}
-                placeholder="Type your guess here.." 
-                className="w-[96%] absolute bottom-[8px] right-0 left-0 rounded-md outline-none p-2 m-auto h-8" />
-        
-            <button 
-            onClick={sendMessage}
-            className="w-5 h-5 bg-blue-500 absolute bottom-3 right-5"></button>
-        
-        </>
-    );
+    // Clear the input field
+    setMessage("");
+  };
+
+  socket.on("received_message", (data) => {
+    // Append the received message to the chat history
+    setChatHistory([...chatHistory, `Stranger: ${data.message}`]);
+  });
+
+  return (
+    <>
+      <div className="w-[96%] h-[75%] absolute top-[8px] right-0 left-0 m-auto bg-white rounded-md">
+        {chatHistory.map((chat, index) => (
+          <div key={index}>{chat}</div>
+        ))}
+      </div>
+      <input
+        type="text"
+        name="chatInput"
+        placeholder="Type your guess here.."
+        className="w-[96%] absolute bottom-[8px] right-0 left-0 rounded-md outline-none p-2 m-auto h-8"
+        value={message}
+        onChange={(e) => setMessage(e.target.value)}
+        onKeyDown={(e) => {
+          if (e.key === "Enter") {
+            sendMessage();
+          }
+        }}
+      />
+      <button 
+      onClick={sendMessage}
+      className="w-5 h-5 bg-blue-500 absolute bottom-3 right-5"></button>
+    </>
+  );
 }
 
 export default Chat;
