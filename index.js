d478fd5777341e8a16fab3d76aef45af6a13115e
Staci082 <125351559+Staci082@users.noreply.github.com>
2023-10-16 21:32:24 +0200

    socket.io message received

--- index.js
+++ index.js
@@ -15,29 +15,22 @@ const io = new Server(server, {
     methods: ["GET", "POST"],
   },
 });
-const onlineUsers = new Map(); 
-
-app.get('/onlineUsers', (req, res) => {
-  res.json(Array.from(onlineUsers.values())); // Convert the map to an array and send it as JSON
-});
 
 
   io.on('connection', (socket) => {
-    // Add the user to the list of online users
-    
-  console.log(`User Connected: ${socket.username}`);
-    socket.on('addUser', (data) => {
-      const { username, avatar } = data;
-      socket.username = username;
-      onlineUsers.set(socket.id, { username, avatar });
+        console.log("user connected:", socket.id)
+
+        socket.on("send_message", (data) => {
+          console.log(data)
+          socket.broadcast.emit("received_message", data)
+        })
+
+        socket.on("disconnect", () => {
+          console.log("user disconnected:", socket.id)
+        })
     });
-  
-    // Remove the user from the list of online users when they disconnect
-    socket.on('disconnect', () => {
-      onlineUsers.delete(socket.id);
-      console.log("User Disconnected", socket.username);
-    });
-  });
+
+
 
 
 
