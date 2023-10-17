30b88155904d7e9be229e7c6ca43e8fda1a789fc
Staci082 <125351559+Staci082@users.noreply.github.com>
2023-10-17 18:50:38 +0200

    onlineUsers cards working realtime

--- index.js
+++ index.js
@@ -25,22 +25,28 @@ io.on("connection", (socket) => {
         } else {
             console.log("new user:", user);
             socket.broadcast.emit("userJoined", {
-              text: `${user.username} has joined the game`,
+                text: `${user.username} has joined the game`,
             });
         }
     });
 
+
     socket.on("message", (data) => {
         socket.broadcast.emit("messageResponse", data);
-        console.log(onlineUsers)
+        console.log(onlineUsers);
     });
 
     socket.on("disconnect", () => {
-        console.log("🔥: A user disconnected");
+        console.log("A user disconnected");
         removeUser(socket.id);
     });
 });
+const emitOnlineUsers = () => {
+    io.emit("onlineUsers", onlineUsers);
+};
 
+
+setInterval(emitOnlineUsers, 1000);
 server.listen(5172, () => {
     console.log("SERVER RUNNING");
 });
