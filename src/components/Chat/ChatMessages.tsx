9023c98f061c26b198db6662e9e74d6338aeebb6
Staci082 <125351559+Staci082@users.noreply.github.com>
2023-10-21 13:32:51 +0200

    joined jioned working

--- src/components/Chat/ChatMessages.tsx
+++ src/components/Chat/ChatMessages.tsx
@@ -27,8 +27,7 @@ function Message({ message, isEven }: MessageProps) {
   const isCurrentUser = message.name === localStorage.getItem("username");
   const messageClass = isEven ? 'px-1' : 'bg-blue-100 px-1';
   const colorClass = message.color;
-  const displayName = isCurrentUser ? 'You' : `${message.name}`;
-  console.log(message.color)
+  const displayName = isCurrentUser ? 'You' : message.name;
   return (
     <p key={message.id} className={`${messageClass} ${colorClass}`}>
     { displayName === undefined ? message.text : `${displayName}: ${message.text}` }
