40a90b8b522d0932df36807213c71ae967237434
Staci082 <125351559+Staci082@users.noreply.github.com>
2023-10-24 20:18:31 +0200

    players unique key added

--- src/components/Players.tsx
+++ src/components/Players.tsx
@@ -31,8 +31,8 @@ export default function Players({ socket }: SocketProp) {
             {onlineUsers
              .filter((user) => user.language === selectedLanguage)
                 .map((user) => (
-                    <div className="my-1 flex items-center justify-center rounded-sm h-12 bg-blue-200">
-                        <div key={user.id} className="flex items-center justify-between w-full mx-2">
+                    <div key={user.id} className="my-1 flex items-center justify-center rounded-sm h-12 bg-blue-200">
+                        <div className="flex items-center justify-between w-full mx-2">
                             <div
                                 style={{
                                     backgroundImage: `url("./pencil.avif")`,
